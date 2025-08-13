4be9f9f6d49a72a0995d2f7902c8b4e08acbd8df
Dmitrii Shalygin <d.shalygin@idaproject.com>
2025-08-14 00:04:19 +0300

    batman

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  sassOptions: {
+    prependData: `@import "@/assets/scss/variables.scss";`
+  }
 };
 
 export default nextConfig;
---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,10 @@
+import styles from './Header.module.scss';
+console.log("e")
+
+export default function Header() {
+    return (
+        <header className={styles.header}>
+
+        </header>
+    )
+}
