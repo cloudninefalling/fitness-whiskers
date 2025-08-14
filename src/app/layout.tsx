aaa56e823eb857d5632c9349d9c04bc52bbe4f59
Dmitrii Shalygin <d.shalygin@idaproject.com>
2025-08-14 20:43:58 +0300

    fix: font import fix

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,28 +6,29 @@ import '@/assets/scss/bundle.scss';
 import Footer from '@/components/layout/Footer/Footer';
 import Header from '@/components/layout/Header/Header';
 
-const RalewayBold = localFont({
-    src: '../../public/fonts/raleway/Raleway-Bold.ttf',
-    weight: '700',
-    style: 'normal',
-});
-
-const RalewaySemibold = localFont({
-    src: '../../public/fonts/raleway/Raleway-Semibold.ttf',
-    weight: '600',
-    style: 'normal',
-});
-
-const RalewayMedium = localFont({
-    src: '../../public/fonts/raleway/Raleway-Medium.ttf',
-    weight: '500',
-    style: 'normal',
-});
-
 const Raleway = localFont({
-    src: '../../public/fonts/raleway/Raleway-Regular.ttf',
-    weight: '400',
-    style: 'normal',
+    src: [
+        { 
+            path: '../../public/fonts/raleway/Raleway-Regular.ttf',
+            weight: '400',
+            style: 'normal', 
+        },
+        {
+            path: '../../public/fonts/raleway/Raleway-Medium.ttf',
+            weight: '500',
+            style: 'normal',
+        },
+        {
+            path: '../../public/fonts/raleway/Raleway-Semibold.ttf',
+            weight: '600',
+            style: 'normal',
+        },
+        {
+            path: '../../public/fonts/raleway/Raleway-Bold.ttf',
+            weight: '700',
+            style: 'normal',
+        },
+    ],
 });
 
 export const metadata: Metadata = {
@@ -36,16 +37,9 @@ export const metadata: Metadata = {
 };
 
 export default function RootLayout({ children }: Readonly<{children: React.ReactNode;}>) {
-    const fonts = [
-        Raleway.className,
-        RalewayBold.className,
-        RalewaySemibold.className,
-        RalewayMedium.className,
-    ].join(' ');
-
     return (
         <html lang="ru">
-            <body className={fonts}>
+            <body className={Raleway.className}>
                 <Header />
 
                 {children}
